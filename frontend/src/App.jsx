134eebf2a116094dfa70758fea5f76bd7a565128
OwensLopez211 <owenslopez211@gmail.com>
2025-05-25 02:36:02 -0400

    agregando speed insight vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Home from "./pages/Home";
 import ContactPage from "./pages/ContactPage";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="contacto" element={<ContactPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="contacto" element={<ContactPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <SpeedInsights />
+    </>
   );
 }
 
